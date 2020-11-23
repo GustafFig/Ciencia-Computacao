744407518217c85b297f197b0926945cce08627f
Gustaff <gustavohmfigueiredo@gmail.com>
2020-11-23 19:37:33 -0300

    done inclusive bonus

--- exercises/35_1/function.py
+++
@@ -1,8 +0,0 @@
-def name(*peso, altura):
-    print("altura", altura)
-    return peso / (altura / 100) ** 2
-
-
-x = name(10, 185, 20)
-
-print(x)
--- exercises/35_1/range.py
+++
@@ -1,6 +0,0 @@
-print(range(10))
-
-dict = {"batata": 1, "framboesa": 2, "margarida": 3}
-
-for i in dict.values():
-    print(i)
